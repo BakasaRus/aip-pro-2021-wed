3ba509a7c6b4381e0e2dd76d9ab2226d22c4ba1a
Nikolay Andreev <bakasaru@list.ru>
2021-10-13 18:38:09 +0300

    Add magic methods

--- 01-Classes/main.py
+++ 01-Classes/main.py
@@ -4,14 +4,22 @@ class Person:
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return f'Person<name={self.name}>'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
     def greet(self):
         print(f'Hi {self.name}!')
 
 
 me = Person('Nick')
+clone = Person('Nick')
 you = Person('Vasya')
 
-print(me.name, you.name)
+print(me, you)
+print(me == clone)
 me.greet()
 you.greet()
 
